54f7ae1ac69276bd2775c07abc99b0fcfe505acd
Rubén Zarroca García <rzarroca@grupointermark.com>
2024-07-25 11:41:53 +0200

    Añadir componente Dashboard con correcciones y localStorage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Dashboard = () => {
